3aec19bb1d924868ac034b16d6ab7606cce98550
Roberto Osorio <roberto.osorio@globant.com>
2018-08-02 10:17:37 -0400

    add readme

---
+++ README.md
@@ -0,0 +1,11 @@
+# Typescript NodeJS Express Backend for Spotify
+
+##Initial Setup
+
+you need add this Environment Variables :
+
+```
+export SPOTIFY_CLIENT_ID=<YourSpotifyClientID>
+export SPOTIFY_CLIENT_SECRET=<YourSpotifyClientID>
+
+```
--- dist/data/api/__mocks__/SpotifyApi.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class MockSpotifyApi {
-    constructor() {
-        this.getAlbums = jest.fn((album, authorization) => { });
-    }
-}
-exports.MockSpotifyApi = MockSpotifyApi;
-//# sourceMappingURL=SpotifyApi.js.map
